4bdeedee619809a3a91b2bd1052592a526ea84b4
N3M3515069 <140887068+N3M3515069@users.noreply.github.com>
2023-07-29 20:24:33 +0530

    Update rotate_pdf.py

--- rotate_pdf.py
+++ rotate_pdf.py
@@ -5,7 +5,7 @@ with open('dummy.pdf', 'rb') as file:
     reader = PyPDF2.PdfFileReader(file)
     page = reader.getPage(0)
     page.rotateClockwise(90)
-    writer = PyPDF2.PdfFileWriter()
+    writer = PyPDF2.PdfFileWriter() #this allows us to write the object in memory
     writer.addPage(page)
-    with open('tilt.pdf', 'wb') as new_file:
+    with open('tilt.pdf', 'wb') as new_file: #to update our dummy pdf to write and rotate
         writer.write(new_file)
