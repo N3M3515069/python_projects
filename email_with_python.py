abae850acacfced4037268b4614cddb5a7fdd0e6
N3M3515069 <140887068+N3M3515069@users.noreply.github.com>
2023-07-30 12:41:58 +0530

    Update email_with_python.py

--- email_with_python.py
+++ email_with_python.py
@@ -7,7 +7,7 @@ email['to'] = 'hellpri3st@gmail.com'
 email['subject'] = 'you have won a Ryzen 5 5600XD with and an AMD Radeon 6750XT '
 
 email.set_content(
-    'Since you got into college and now you need a new midrange PC hereare the free parts for that , buy the rest by yourself')
+    'Since you got into college and now you need a new midrange PC here are the free parts for that , buy the rest by yourself')
 
 # now using this we will login into our gmail to send the email
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
